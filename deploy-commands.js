dfb1471630e430f32d810729f2e66f5c9471c936
marcsaradin <msaradin518@gmail.com>
2026-04-06 19:56:58 +0200

    Add files via upload

---
+++ deploy-commands.js
@@ -0,0 +1,26 @@
+const { REST, Routes } = require('discord.js');
+const fs = require('fs');
+require('dotenv').config();
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    if (command.data) commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Déploiement des commandes slash...');
+        await rest.put(
+            Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID),
+            { body: commands }
+        );
+        console.log('✅ Commandes slash déployées !');
+    } catch (error) {
+        console.error(error);
+    }
+})();
