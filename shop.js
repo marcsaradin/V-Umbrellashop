7c57709aeafa4dffe09a1660e902b3514d5cc736
vangaming_yt <msaradin518@gmail.com>
2026-04-10 02:32:57 +0200

    Add files via upload

--- shop.js
+++ shop.js
@@ -1,15 +1,15 @@
 const { SlashCommandBuilder } = require('discord.js');
 
-const shopURL = "https://marcsaradin.github.io/V-Umbrellashop/shop.html";
+const shopURL = "https://v-umbrellashop-production.up.railway.app";
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('shop')
-        .setDescription('Ouvre le shop V-Umbrella'),
+  data: new SlashCommandBuilder()
+    .setName('shop')
+    .setDescription('Ouvre le shop V-Umbrella'),
 
-    async execute(interaction) {
-        await interaction.reply({
-            content: `🛒 **V-Umbrella Shop**\n🔗 Clique ici pour accéder au shop :\n${shopURL}`
-        });
-    }
+  async execute(interaction) {
+    await interaction.reply({
+      content: `🛒 **V-Umbrella Shop**\n🔗 ${shopURL}`
+    });
+  }
 };
